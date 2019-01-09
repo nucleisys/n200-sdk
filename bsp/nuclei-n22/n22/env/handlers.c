a53fc2256f258d90ca268704c0ba648940762e2a
angellababy <angella_baby@163.com>
2019-01-09 00:45:35 -0800

    update some CSR address and accordingly update the relevant handlers and start.S

--- bsp/nuclei-n22/n22/env/handlers.c
+++ bsp/nuclei-n22/n22/env/handlers.c
@@ -5,8 +5,19 @@
 #include "n22/drivers/riscv_encoding.h"
 #include "n22/drivers/n22_func.h"
 
+__attribute__((weak)) uintptr_t handle_nmi()
+{
+  write(1, "nmi\n", 5);
+  _exit(1);
+  return 0;
+}
+
+
 __attribute__((weak)) uintptr_t handle_trap(uintptr_t mcause, uintptr_t sp)
 {
+  if(mcause == 0x1001) {
+      handle_nmi;
+  }
   write(1, "trap\n", 5);
   printf("In trap handler, the mcause is %d\n", mcause);
   printf("In trap handler, the mepc is 0x%x\n", read_csr(mepc));
@@ -16,12 +27,5 @@ __attribute__((weak)) uintptr_t handle_trap(uintptr_t mcause, uintptr_t sp)
 }
 
 
-
-__attribute__((weak)) uintptr_t handle_nmi()
-{
-  write(1, "nmi\n", 5);
-  _exit(1);
-  return 0;
-}
 
 
