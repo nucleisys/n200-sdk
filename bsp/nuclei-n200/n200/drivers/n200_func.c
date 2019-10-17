a6229afd3fa849e294dceb2dd80b37272355c805
WsHelloWorld <3265302782qq.com>
2019-10-17 16:48:54 +0800

    fix the setting of negedge_trig

--- bsp/nuclei-n200/n200/drivers/n200_func.c
+++ bsp/nuclei-n200/n200/drivers/n200_func.c
@@ -342,7 +342,7 @@ void eclic_set_negedge_trig(uint32_t source) {
       // Keep other bits unchanged and only set the bit 1
   uint8_t new_intattr = (old_intattr | 0x2);
       // Keep other bits unchanged and only set the bit 2
-  new_intattr = (old_intattr | 0x4);
+  new_intattr = (new_intattr | 0x4);
 
   eclic_set_intattr(source,new_intattr);
 }
